1cb6dc45285fcd08c9044601e7934cad6635787c
Boris Tityukov <boristityukov@gmail.com>
2020-08-27 11:12:00 +0300

    feat: add a playlist on the home page

--- src/components/playlist/index.js
+++ src/components/playlist/index.js
@@ -1,17 +1,18 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import styles from './index.module.css'
 
 const Playlist = () => {
 
-    // useEffect(() => {
-    //     const getPlaylist = async () => {
-            
-    //     }
-
-    //     const result = getPlaylist()
-    // }, [])
-
     return (
-        <p>Playlist</p>
+        <div className={styles.player}>
+            <iframe
+                title="Spotify"
+                src="https://embed.spotify.com/?uri=spotify:album:7gNavsehRiz2rpxLq5GnsL?si=mAPMpp_xQdyikCIq8vZ-3Q&amp;view=list&amp;theme=white"
+                frameBorder="0"
+                allowtransparency="true"
+                allow="geolocation *">
+            </iframe>
+        </div>
     )
 }
 
